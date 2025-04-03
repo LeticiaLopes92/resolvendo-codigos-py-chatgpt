e69d4ba523c9e1c4bba575071b2f7cf530e24c3d
Leticia Lopes <162375644+LeticiaLopes92@users.noreply.github.com>
2025-04-03 06:48:16 +0000

    ajuste

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -4,16 +4,12 @@ num2 = int(input("Digite o segundo número: "))
 
 operacao = input("Digite a operação desejada (+, -, *, /): ")
 if operacao == "+":
-    print (num1 + num2)
+    print(num1 + num2)
 elif operacao == "-":
-   print (abs(num1 - num2))
+   print(abs(num1 - num2))
 elif operacao == "*":
-    print (num1 * num2)
+    print(num1 * num2)
 elif operacao == "/":
-    if num2 != 0:
         print (num1 / num2)
-    else:
-        resultado = "Erro: Divisão por zero não é permitida."
 else:
-    resultado = "Erro: Operação inválida."
-print(f"O resultado da operação {num1} {operacao} {num2} é: {resultado}")
+     print("Operação inválida")
