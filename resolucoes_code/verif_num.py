3a37e63e2a18855f0bfe24d0b88f51910f5ad743
Leticia Lopes <162375644+LeticiaLopes92@users.noreply.github.com>
2025-04-03 07:02:49 +0000

    resolução

--- resolucoes_code/verif_num.py
+++ resolucoes_code/verif_num.py
@@ -1,3 +1,11 @@
 # Como entrada, receba um número inteiro e verifique se ele é par ou ímpar. Uma dica é: Utilize condicionais para realizar a verificação e, se possível, faça uso do Github Copilot(ou outra IA) para otimizar a estrutura do código.
 
+# Vamos solicitar como entrada um número inteiro e verificar se ele é par ou ímpar.
+numero = int(input("Digite um número inteiro: "))
+if numero % 2 == 0:
+    print(f"{numero} é par.")
+else:
+    print(f"{numero} é ímpar.")
+
+
 
