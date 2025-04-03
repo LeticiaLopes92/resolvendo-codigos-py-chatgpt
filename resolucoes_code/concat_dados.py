7a89d92ebbce7f16483b705834a2a8f02ba1053d
Leticia Lopes <162375644+LeticiaLopes92@users.noreply.github.com>
2025-04-03 06:14:39 +0000

    espaço

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -3,7 +3,7 @@
 info1 = input("Digite a primeira informação: ")
 info2 = input("Digite a segunda informação: ")
 
-info_concatenada = info1 + "" + info2
+info_concatenada = info1 + " " + info2
 
 print("As informações concatenadas são:", info_concatenada)
 
